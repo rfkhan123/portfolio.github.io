81a6a08091c319ef8a2dd893577de7ed0f9605e9
rfkhan123 <105058323+rfkhan123@users.noreply.github.com>
2024-07-08 16:33:09 +0530

    Update K_Means.py

--- Algorithm_Implementations/K_Means.py
+++ Algorithm_Implementations/K_Means.py
@@ -1,22 +1,16 @@
 from sklearn.cluster import KMeans
 import numpy as np
 
-# Example engagement data (replace with your actual data)
 engagement_data = np.array([[10], [15], [20], [25], [30], [35], [40]])
 
-# Number of clusters (K)
 num_clusters = 2
 
-# Create a K-means clustering model
 kmeans = KMeans(n_clusters=num_clusters)
 
-# Fit the model to the data
 kmeans.fit(engagement_data)
 
-# Get cluster labels and centroids
 cluster_labels = kmeans.labels_
 centroids = kmeans.cluster_centers_
 
-# Print results
 print("Cluster Labels:", cluster_labels)
 print("Centroids:", centroids)
